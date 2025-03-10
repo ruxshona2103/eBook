df5aca2fdd28b26bb87721600daf07a7c008a7d9
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-03-10 10:14:39 +0500

    apps and admin.py added

--- ebook_app/admin.py
+++ ebook_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from ebook_app.models import *
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Order)
+
--- ebook_app/apps.py
+++ ebook_app/apps.py
@@ -5,3 +5,5 @@ class EbookAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ebook_app'
 
+    def ready(self):
+        import ebook_app.signals
